dab386b33edf962e81576c4a20eb2f6dac7f3049
hayleystevens <hayley.stevens@slalom.com>
2019-02-19 14:03:52 -0800

    update alterys

--- extension_test.js
+++ extension_test.js
@@ -85,10 +85,10 @@
   
           return rowData;
         });
-        alert("0.0 "+text(data[0][0]));
-        alert("1.0 "+text(data[1][0]));
-        alert("0.1 "+text(data[0][1]));
-        alert("1.1 "+text(data[1][1]));  
+        alert("0.0 "+(data[0][0]));
+        alert("1.0 "+(data[1][0]));
+        alert("0.1 "+(data[0][1]));
+        alert("1.1 "+(data[1][1]));  
       $('#Platform').text(data[0][0]);
       $('#FBLink').text("https://www.facebook.com/"+data[0][1]);
       $('#VideoID').text(data[0][1]);      
