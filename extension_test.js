6b88561cc0d064f8b0b820e9264a562d47c3d155
hayleystevens <hayley.stevens@slalom.com>
2019-02-19 09:23:36 -0800

    updated

--- extension_test.js
+++ extension_test.js
@@ -37,7 +37,7 @@
 
         // Close the dialog and show the data table for this worksheet
         $('#choose_sheet_dialog').modal('toggle');
-        loadSelectedMarks(worksheetName);
+        LoadMarks(worksheetName);
       });
 
       // Add our button to the list of worksheets to choose from
@@ -60,7 +60,7 @@
   // This variable will save off the function we can call to unregister listening to marks-selected events
   let unregisterEventHandlerFunction;
 
-  function loadSelectedMarks (worksheetName) {
+  function LoadMarks (worksheetName) {
     // Remove any existing event listeners
     if (unregisterEventHandlerFunction) {
       unregisterEventHandlerFunction();
@@ -74,16 +74,38 @@
     alert(worksheet.name);
     
     // Call to get the selected marks for our sheet
-    worksheet.getSummaryDataAsync().then(function(t){  
-      var rawData = t.getData();  
- }); 
- alert(rawData); 
+    worksheet.getSummaryDataAsync().then(reportMarks);  
+
+    function reportMarks(marks) {
+      var html = "";
+      if (marks.length == 0)
+
+      alert("selectedMarks: empty list");
     
+      else {
+    
+        const worksheetData = marks.data[0];
+
+        // Map our data into the format which the data table component expects it
+        const data = worksheetData.data.map(function (row, index) {
+          const rowData = row.map(function (cell) {
+            return cell.formattedValue;
+          });
+  
+          return rowData;
+        });
+      // alert("Mark1" + marks.data[0]);
+      // alert("Mark2" + data);
+      alert("Mark3" + data[0][0]);
+      $('#returnID-Title').text((data[0][0]));
+      }
+          };
+
 
     // Add an event listener for the selection changed event on this sheet.
     unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.FilterChanged, function (filterEvent) {
       // When the selection changes, reload the data
-      loadSelectedMarks(worksheetName);
+      loadMarks(worksheetName);
     });
   }
 
@@ -97,4 +119,4 @@
       return sheet.name === worksheetName;
     });
   }
-})();
+}})();
