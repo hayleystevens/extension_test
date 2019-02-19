8b7d471b8e363e73347ba6aaf2e1a7c3c5660878
hayleystevens <hayley.stevens@slalom.com>
2019-02-19 15:45:46 -0800

    updates

--- extension_test.js
+++ extension_test.js
@@ -86,7 +86,7 @@
           return rowData;
      
         });
-        alert('rowdata'+rowData);
+        alert('rowdata'+text(rowData));
       $('#Platform').text((data[0][0]));
       $('#VideoID').text((data[0][-1]));
       alert("Platform" + data[0][0]);
