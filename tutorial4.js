9724829bb3285c244902746ab58b6b2329b15533
hayleystevens <hayley.stevens@slalom.com>
2019-02-19 10:45:38 -0800

    updates

--- tutorial4.js
+++ tutorial4.js
@@ -13,7 +13,9 @@
     });
   });
 
-  //Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
+  /**
+   * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
+   */
   function showChooseSheetDialog () {
     // Clear out the existing list of sheets
     $('#choose_sheet_buttons').empty();
@@ -65,26 +67,69 @@
     if (unregisterEventHandlerFunction) {
       unregisterEventHandlerFunction();
     }
- 
-     // Get the worksheet object we want to get the selected marks for
+
+    // Get the worksheet object we want to get the selected marks for
     const worksheet = getSelectedSheet(worksheetName);
 
     // Set our title to an appropriate value
     $('#selected_marks_title').text(worksheet.name);
-    alert(worksheet.name);
-   
 
- // get the summary data for the sheet
- worksheet.getSummaryDataAsync().then(function (sumdata) {
-  const worksheetData = sumdata;
- });
- alert("sumdata"+sumdata);
+    // Call to get the selected marks for our sheet
+    worksheet.getSelectedMarksAsync().then(function (marks) {
+      // Get the first DataTable for our selected marks (usually there is just one)
+      const worksheetData = marks.data[0];
 
-     // Add an event listener for the selection changed event on this sheet.
-     unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.FilterChanged, function (selectionEvent) {
+      // Map our data into the format which the data table component expects it
+      const data = worksheetData.data.map(function (row, index) {
+        const rowData = row.map(function (cell) {
+          return cell.formattedValue;
+        });
+
+        return rowData;
+      });
+
+      const columns = worksheetData.columns.map(function (column) {
+        return { title: column.fieldName };
+      });
+
+      // Populate the data table with the rows and columns we just pulled out
+      populateDataTable(data, columns);
+    });
+
+    // Add an event listener for the selection changed event on this sheet.
+    unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, function (selectionEvent) {
       // When the selection changes, reload the data
       loadSelectedMarks(worksheetName);
     });
+  }
+
+  function populateDataTable (data, columns) {
+    // Do some UI setup here to change the visible section and reinitialize the table
+    $('#data_table_wrapper').empty();
+
+    if (data.length > 0) {
+      $('#no_data_message').css('display', 'none');
+      $('#data_table_wrapper').append(`<table id='data_table' class='table table-striped table-bordered'></table>`);
+
+      // Do some math to compute the height we want the data table to be
+      var top = $('#data_table_wrapper')[0].getBoundingClientRect().top;
+      var height = $(document).height() - top - 130;
+
+      // Initialize our data table with what we just gathered
+      $('#data_table').DataTable({
+        data: data,
+        columns: columns,
+        autoWidth: false,
+        deferRender: true,
+        scroller: true,
+        scrollY: height,
+        scrollX: true,
+        dom: "<'row'<'col-sm-6'i><'col-sm-6'f>><'row'<'col-sm-12'tr>>" // Do some custom styling
+      });
+    } else {
+      // If we didn't get any rows back, there must be no marks selected
+      $('#no_data_message').css('display', 'inline');
+    }
   }
 
   function initializeButtons () {
