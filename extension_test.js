73cf5c65c963c719b7a76dabd26e7a25c72a8db2
hayleystevens <hayley.stevens@slalom.com>
2019-02-18 18:17:25 -0800

    major revisions

--- extension_test.js
+++ extension_test.js
@@ -75,61 +75,33 @@
     $('#selected_marks_title').text(worksheet.name);
 
     // Call to get the selected marks for our sheet
-    worksheet.getSelectedMarksAsync().then(function (marks) {
-      // Get the first DataTable for our selected marks (usually there is just one)
-      const worksheetData = marks.data[0];
+    worksheet.getSelectedMarksAsync().then(reportSelectedMarks);
 
-      // Map our data into the format which the data table component expects it
-      const data = worksheetData.data.map(function (row, index) {
-        const rowData = row.map(function (cell) {
-          return cell.formattedValue;
-        });
+  function reportSelectedMarks(marks) {
+      var html = "";
 
-        return rowData;
-      });
+      for (var markIndex = 0; markIndex < marks.length; markIndex++) {
+          var pairs = marks[markIndex].getPairs();
+          html += "<b>Mark " + markIndex + ":</b><ul>";
 
-      const columns = worksheetData.columns.map(function (column) {
-        return { title: column.fieldName };
-      });
+          for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
+              var pair = pairs[pairIndex];
+              html += "<li><b>Field Name:</b> " + pair.fieldName;
+              html += "<br/><b>Value:</b> " + pair.formattedValue + "</li>";
+          }
 
-      // Populate the data table with the rows and columns we just pulled out
-      populateDataTable(data, columns);
-    });
+          html += "</ul>";
+      }
+
+      var infoDiv = document.getElementById('markDetails');
+      infoDiv.innerHTML = html;
+  }
 
     // Add an event listener for the selection changed event on this sheet.
     unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, function (selectionEvent) {
       // When the selection changes, reload the data
       loadSelectedMarks(worksheetName);
     });
-  }
-
-  function populateDataTable (data, columns) {
-    // Do some UI setup here to change the visible section and reinitialize the table
-    $('#data_table_wrapper').empty();
-
-    if (data.length > 0) {
-      $('#no_data_message').css('display', 'none');
-      $('#data_table_wrapper').append(`<table id='data_table' class='table table-striped table-bordered'></table>`);
-
-      // Do some math to compute the height we want the data table to be
-      var top = $('#data_table_wrapper')[0].getBoundingClientRect().top;
-      var height = $(document).height() - top - 130;
-
-      // Initialize our data table with what we just gathered
-      $('#data_table').DataTable({
-        data: data,
-        columns: columns,
-        autoWidth: false,
-        deferRender: true,
-        scroller: true,
-        scrollY: height,
-        scrollX: true,
-        dom: "<'row'<'col-sm-6'i><'col-sm-6'f>><'row'<'col-sm-12'tr>>" // Do some custom styling
-      });
-    } else {
-      // If we didn't get any rows back, there must be no marks selected
-      $('#no_data_message').css('display', 'inline');
-    }
   }
 
   function initializeButtons () {
@@ -143,3 +115,4 @@
     });
   }
 })();
+
