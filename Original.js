6c60a9dc9381fbc4f187de28b9d1ca7fe6b0c4f2
hayleystevens <hayley.stevens@slalom.com>
2019-02-19 09:14:34 -0800

    updated

--- Original.js
+++ Original.js
@@ -13,7 +13,9 @@
     });
   });
 
-  //Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
+  /**
+   * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
+   */
   function showChooseSheetDialog () {
     // Clear out the existing list of sheets
     $('#choose_sheet_buttons').empty();
@@ -71,23 +73,71 @@
 
     // Set our title to an appropriate value
     $('#selected_marks_title').text(worksheet.name);
-    
-    // Call to get the selected marks for our sheet
-    worksheet.getSummaryDataAsync().then(function(t){  
-      var rawData = t.getData();  
- }); 
- alert(rawData);
 
+    // Call to get the selected marks for our sheet
+
+    tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === worksheet.name).getSelectedMarksAsync().then(reportSelectedMarks);
+
+    // worksheet.getSelectedMarksAsync().then(reportSelectedMarks);
+
+  function reportSelectedMarks(marks) {
+      var html = "";
+      if (marks.length == 0)
+
+      alert("selectedMarks: empty list");
+    
+      else {
+    
+        const worksheetData = marks.data[0];
+
+        // Map our data into the format which the data table component expects it
+        const data = worksheetData.data.map(function (row, index) {
+          const rowData = row.map(function (cell) {
+            return cell.formattedValue;
+          });
   
+          return rowData;
+        });
       // alert("Mark1" + marks.data[0]);
       // alert("Mark2" + data);
       alert("Mark3" + data[0][0]);
-      $('#returnID-Title').text((data[0][0]));
-      };      
+      $('#returnID-Title').text(filtervalues(data[0][0]));
+      }
+      function filtervalues(val){
+        if(val == "West"){
+          return "this is awesome"
+        }
+        else{
+          return "The rest of the country"
+        }
+      };
     
+      // alert(alertOutput);
+    
+      // for (var markIndex = 0; markIndex < marks.length; markIndex++) {
+      //     var pairs = marks[markIndex].getPairs();
+
+      //     for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
+      //         var pair = pairs[pairIndex];
+      //         if (pair.fieldName="ID")
+      //         {
+      //           var found_ID=pair.formattedValue;
+      //           return found_ID;
+      //         }
+      //         else{
+      //           var found_ID = "could not find";
+      //           return found_ID;
+      //         }
+      //       }
+           
+
+      // }
+  // }
+
+  // $('#returnID').text(found_ID);
 
     // Add an event listener for the selection changed event on this sheet.
-    unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.FilterChanged, function (filterEvent) {
+    unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, function (selectionEvent) {
       // When the selection changes, reload the data
       loadSelectedMarks(worksheetName);
     });
